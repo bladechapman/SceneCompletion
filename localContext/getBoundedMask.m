257a74b22998f1c8e816defdbb0c9b78f160e904
bladechapman <bechapm2@illinois.edu>
2015-12-12 15:52:21 -0600

    ssd formula

--- localContext/getBoundedMask.m
+++ localContext/getBoundedMask.m
@@ -2,7 +2,7 @@ function [ bounded_mask ] = getBoundedMask( original_mask )
 %GETMASKBOUNDINGBOX Summary of this function goes here
 %   Detailed explanation goes here
 %   Currently only works on inclusive masks
-    [original_height, original_width] = size(original_mask);
+    [original_height, original_width, ~] = size(original_mask);
     min_x = original_width + 1;
     min_y = original_height + 1;
     max_x = 0;
@@ -12,7 +12,7 @@ function [ bounded_mask ] = getBoundedMask( original_mask )
         for x=1:original_width
             original_mask_value = original_mask(y, x);
             
-            if (original_mask_value == 1)
+            if (original_mask_value > 0)
                 if x > max_x
                     max_x = x;
                 end
@@ -32,7 +32,7 @@ function [ bounded_mask ] = getBoundedMask( original_mask )
         end
     end
     
-    bounded_mask = original_mask(min_y:max_y, min_x:max_x);
+    bounded_mask = original_mask(min_y:max_y, min_x:max_x, :);
     
 end
 
