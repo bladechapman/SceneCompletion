aee30b42941c2907a71def4948f65b6e85e424f7
bladechapman <bechapm2@illinois.edu>
2015-12-12 17:07:23 -0600

    cleaned up

--- test.m
+++ test.m
@@ -5,13 +5,9 @@ addpath localContext/
 test_im = im2double(imread('./test_images/test_3.jpg'));
 test_im_2 = im2double(imread('./test_images/test_4.jpg'));
 mask_include = getMask(test_im);
-
 mask_exclude = imcomplement(mask_include);
 
-
-%% test poisson blend
-% context_mask = getContextMask(mask_include);
-% calculate mask of area surrounding the hole (80px radius)
+%% test determine best patch placements test
 context_mask = getContextMask(mask_include);
 patch = placeContext(test_im, test_im_2, context_mask);
-% figure(2), imagesc(patch);
+figure(2), imagesc(patch);
