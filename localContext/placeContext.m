e65a57da4da684065267c5699ca4444250aabfd3
bladechapman <bechapm2@illinois.edu>
2015-12-12 15:13:26 -0600

    linearization v1

--- localContext/placeContext.m
+++ localContext/placeContext.m
@@ -2,39 +2,60 @@ function [ best_patch ] = placeContext( source_image, replacement_image, context
 %PLACECONTEXT Summary of this function goes here
 %   Detailed explanation goes here
 
+% figure(1), imshow(source_image);
+% figure(2), imshow(replacement_image);
+% figure(3), imshow(context_mask);
+% pause;
+
 % assume source and replacement images are same dimensions
 ssd = zeros(size(replacement_image));
 ssd(:,:,:) = intmax;
-[height, width, c] = size(replacement_image);
-[mask_height, mask_width, m_c] = size(context_mask);
+[height, width, ~] = size(replacement_image);
+[mask_height, mask_width, ~] = size(context_mask);
+
+rgb_mask = repmat(context_mask, [1,1,3]);
 
 % slide this mask across the entire image and compute SSD at each pixel
-for y = 1:height-mask_height+1
-    for x = 1:width-mask_width+1
-        disp([y x]);
-        source_image_rect = source_image(y:y+mask_height-1, x:x+mask_width-1, :);
-        replacement_image_rect = replacement_image(y:y+mask_height-1, x:x+mask_width-1, :);
-        source_image_region = source_image_rect(:,:,:) .* repmat(context_mask, [1,1,3]);
-        replacement_image_region = replacement_image_rect(:,:,:) .* repmat(context_mask, [1,1,3]);
-        
-        ssd(y, x) = sum(sum(sum((replacement_image_region - source_image_region).^2)));
-        
-    end
-end
+% I = source image
+% M = mask
+% T = template
 
-% ssd(1:height-mask_height+1, 1:width-mask_width+1) = sum(sum(sum((...
-%     replacement_image(...
-%         (1:height-mask_height):(1:height-mask_height+1)+mask_height-1,...
-%         (1:width-mask_width):(1:width-mask_width+1)+mask_width-1,...
-%         :).* repmat(context_mask, [1,1,3]) - ...
-%     source_image(...
-%         (1:height-mask_height):(1:height-mask_height+1)+mask_height-1,...
-%         (1:width-mask_width):(1:width-mask_width+1)+mask_width-1,...
-%         :).* repmat(context_mask, [1,1,3])).^2)));
+disp('beginning computation');
+tic
+ssd = imfilter(source_image.^2, context_mask);
+    - 2*(imfilter(source_image, context_mask).*imfilter(replacement_image,context_mask))
+    + imfilter(replacement_image.^2, context_mask);
+disp('end computation');
+disp(toc)
+figure(10), imagesc(mat2gray(ssd));
+pause;
+
+% disp('begin old comp');
+% for y = 1:(height - mask_height + 1)
+%     disp(y);
+%     for x = 1:(width - mask_width + 1)
+%         
+%         
+%         source_image_rect = source_image(y:y+mask_height-1, x:x+mask_width-1, :);
+%         
+%         replacement_image_rect = replacement_image(y:y+mask_height-1, x:x+mask_width-1, :);
+%         
+%         
+%         source_image_region = source_image_rect(:,:,:) .* rgb_mask;
+%         replacement_image_region = replacement_image_rect(:,:,:) .* rgb_mask;
+%         
+%         ssd(y, x) = sum(sum(sum((replacement_image_region - source_image_region).^2)));
+%     end
+% end
+% disp('end old comp')
+% 
+% imshow(mat2gray(ssd));
+% pause;
 
 % find starting pixel with smallest ssd
-[M, I] = min(ssd(:));
-[best_y, best_x] = ind2sub(size(ssd), I);
-best_patch = replacement_image(best_y:best_y + mask_height, best_x + mask_width);
+% [M, I] = min(ssd(:));
+% [best_y, best_x] = ind2sub(size(ssd), I);
+% best_patch = replacement_image(best_y:best_y + mask_height, best_x + mask_width);
+best_patch = 0;
 end
 
--- test.m
+++ test.m
@@ -4,16 +4,18 @@ addpath localContext/
 %% test mask generation
 test_im = im2double(imread('./test_images/test_1.png'));
 test_im_2 = im2double(imread('./test_images/test_2.jpg'));
-% mask_include = getMask(test_im);
-% mask_exclude = imcomplement(mask_include);
-% bounded_inclusive_mask = getBoundedMask(mask_include);
+mask_include = getMask(test_im);
+mask_exclude = imcomplement(mask_include);
+bounded_inclusive_mask = getBoundedMask(mask_include);
 
 %% test poisson blend
 
 % context_mask = getContextMask(mask_include);
 % calculate mask of area surrounding the hole (80px radius)
-% context_mask = getContextMask(mask_include);
-% context_mask = getBoundedMask(context_mask);
+context_mask = getContextMask(mask_include);
+context_mask = getBoundedMask(context_mask);
+
 patch = placeContext(test_im, test_im_2, context_mask);
 
-figure(2), imagesc(patch);
+disp(patch)
+% figure(2), imagesc(patch);
