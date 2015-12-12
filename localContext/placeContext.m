86454d1d7df626aa16427510999d2cc1e52b5c14
xColonelPanic <agadboi2@illinois.edu>
2015-12-12 16:09:05 -0600

    LAB

--- localContext/placeContext.m
+++ localContext/placeContext.m
@@ -17,18 +17,20 @@ rgb_mask = repmat(context_mask, [1,1,3]);
 bounded_mask = getBoundedMask(context_mask);
 bounded_rgb_mask = repmat(bounded_mask, [1,1,3]);
 template = getBoundedMask(source_image .* rgb_mask);
+replacement_image_lab = rgb2lab(replacement_image(:,:,1), replacement_image(:,:,2), replacement_image(:,:,3));
+template_lab = rgb2lab(template(:,:,1), template(:,:,2), template(:,:,3));
 
 figure(1), imshow(replacement_image);
 figure(2), imshow(bounded_rgb_mask);
 figure(3), imshow(template);
-pause
+% pause
 
 % ssd = imfilter(I.^2, M) -2*imfilter(I, M.*T) + sum(sum((M.*T).^2))
 disp('beginning computation');
 tic
-ssd = imfilter(replacement_image(:, :, 1).^2, bounded_mask)...
-    - 2*imfilter(replacement_image(:, :, 1), template(:, :, 1))...
-    + sum(sum((template(:, :, 1)).^2));
+ssd = imfilter(replacement_image_lab(1).^2, bounded_mask)...
+    - 2*imfilter(replacement_image_lab(:, :, 1), template_lab(:, :, 1))...
+    + sum(sum((template_lab(:, :, 1)).^2));
 disp('end computation');
 disp(toc)
 figure(10), imagesc(mat2gray(ssd));
