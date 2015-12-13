210a6566b7a9df5a5b834a1b27760e0fcb140e8b
xColonelPanic <agadboi2@illinois.edu>
2015-12-13 14:00:15 -0600

    skeleton

---
+++ pipeline.m
@@ -0,0 +1,17 @@
+%% generate gist descriptors for all images
+
+%% ssd all gist descriptors with gist of source image
+ % to get top X best matchs
+ 
+%% compute mask, computer score for each top X images
+ 
+%% computer best patch
+
+%% graph cut
+
+%% poisson blend
+
+%% output image
+
+%% 
+%% profit
