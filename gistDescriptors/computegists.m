93928cb5420f2bcefc720df17dff5867f35d1a67
bladechapman <bechapm2@illinois.edu>
2015-12-13 21:14:58 -0600

    updated computegists stuff

--- gistDescriptors/computegists.m
+++ gistDescriptors/computegists.m
@@ -2,14 +2,15 @@ function computegists
 % given a large set of images, generates gist descriptors for each image in
 % the set, saving that file to disk so that the computation only needs to
 % be done once. cool? cool.
+addpath images/
 
 %file extension stuff so we can just chuck the images id into it
-image_name_prefix =    './im';
+image_name_prefix =    './images/im';
 image_name_extension = '.jpg';
 
 %range of the image ids that you wanna work with
-start = 1;
-finish = 7;
+start = 7000;
+finish = 13999;
 
 %%%%%%% computation follows below %%%%%%%%
 
@@ -27,12 +28,19 @@ gist = zeros([(finish-start+1) Nfeatures]);
 img = imread(sprintf('%s%05d%s', image_name_prefix, start, image_name_extension));
 [gist(1, :), param] = LMgist(img, '', param); % first call
 % Loop:
+tic
 for i = (start+1):finish
    img = imread(sprintf('%s%05d%s', image_name_prefix, i, image_name_extension));
    gist(i - start + 1, :) = LMgist(img, '', param); % the next calls will be faster
+   if mod(i,250)==0
+       disp(i);
+       save('gistvalues', 'gist');
+       toc
+       tic
+   end
 end
+toc
 
 save('gistvalues', 'gist');
 
 end
-
